6bdec982a13d51e89a064b1f16121e46e4d5089e
alcazarstore27-oss <alcazar.store27@gmail.com>
2026-05-06 21:43:47 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-console.log("App iniciada");
+const guardarBtn = document.getElementById("guardarBtn");
+const tablaBody = document.getElementById("tablaBody");
+
+guardarBtn.addEventListener("click", () => {
+
+    const rastreo = document.getElementById("rastreo").value;
+    const contenido = document.getElementById("contenido").value;
+    const estado = document.getElementById("estado").value;
+    const fecha = document.getElementById("fecha").value;
+    const proveedor = document.getElementById("proveedor").value;
+
+    if(rastreo === "" || contenido === ""){
+        alert("Completa los campos obligatorios");
+        return;
+    }
+
+    const nuevaFila = `
+        <tr>
+            <td>${rastreo}</td>
+            <td>${contenido}</td>
+            <td>${estado}</td>
+            <td>${fecha}</td>
+            <td>${proveedor}</td>
+        </tr>
+    `;
+
+    tablaBody.innerHTML += nuevaFila;
+
+    // Limpiar formulario
+    document.getElementById("rastreo").value = "";
+    document.getElementById("contenido").value = "";
+    document.getElementById("proveedor").value = "";
+    document.getElementById("observaciones").value = "";
+
+});
